361f7abe03daa4b524ba54f0f96b12fe720dface
russofel <russofelix1@yahoo.at>
2023-12-07 10:18:45 +0100

    comments and format

--- visualization_scripts/gaussian.py
+++ visualization_scripts/gaussian.py
@@ -1,41 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Impact parameter covariance
 covs = np.loadtxt("/home/frusso/hep/out/gaussian_plot/cov.txt")
 covs = covs.reshape((-1, 3, 3))
+# Impact parameters
 ips = np.loadtxt("/home/frusso/hep/out/gaussian_plot/ip.txt")
 ips = ips.reshape((-1, 3))
 
-#print(np.mean(covs, axis=0))
+# Indices of maximum (co)variance
 max_inds = np.argmax(covs, axis=0)
+
+# Print covariance matrix and impact parameters corresponding to the highest
+# var(T)
 print(covs[max_inds[2, 2]])
 print(ips[max_inds[2, 2]])
 
-ipParams = np.array([0.008389, -43.8941, -346.304])
-cov = np.array( [[0.011,         -1.4928e-06,  -1.95788e-05],
-                 [-1.4928e-06,   0.5,         -3.18842e-05],#0.5, 3.0
-                 [-1.95788e-05, -3.18842e-05, 208.]])
+# choose a covariance matrix and impact parameters to plot
+plotIndex = 30
+ip = ips[plotIndex]
+cov = covs[plotIndex]
 
+# Algorithm parameters
 zBinExtent = 0.05
 tBinExtent = 19
-nZVals = 41#9
+nZVals = 41  # 9
 nTVals = 5
 
-def find_nearest(array, value):
-    array = np.asarray(array)
-    idx = (np.abs(array - value)).argmin()
-    return idx
 
 def f(vals):
-    diff = vals - ipParams
-    res = 1/np.sqrt(np.linalg.det(cov)) * np.exp(-0.5 * diff.transpose().dot(np.linalg.inv(cov) @ diff))
+    diff = vals - ip
+    res = (
+        1
+        / np.sqrt(np.linalg.det(cov))
+        * np.exp(-0.5 * diff.transpose().dot(np.linalg.inv(cov) @ diff))
+    )
     return res
 
+
 def zValFromBin(bin):
-    return ipParams[1] + zBinExtent * (bin - nZVals / 2)
+    return ip[1] + zBinExtent * (bin - nZVals / 2)
+
 
 def tValFromBin(bin):
-    return ipParams[2] + tBinExtent * (bin - nTVals / 2)
+    return ip[2] + tBinExtent * (bin - nTVals / 2)
+
 
 densities = np.zeros((nZVals, nTVals))
 for zBin in range(nZVals):
